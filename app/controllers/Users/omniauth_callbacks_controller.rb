fe915964f964c64de383673c099bfa120a077c3a
Yain Porter <144638203+yainporter@users.noreply.github.com>
2024-04-25 22:28:47 -0700

    feat: Add back :authenticate_user
    a

--- app/controllers/Users/omniauth_callbacks_controller.rb
+++ app/controllers/Users/omniauth_callbacks_controller.rb
@@ -1,20 +1,19 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def passthru
-    require 'pry'; binding.pry
     super
   end
 
   def google_oauth2
-    require 'pry'; binding.pry
+    client = Signet::OAuth2::Client.new(client_options)
       # You need to implement the method below in your model (e.g. app/models/user.rb)
-      @user = User.from_omniauth(request.env['omniauth.auth'])
-      if @user.persisted?
-        flash[:notice] = I18n.t 'devise.omniauth_callbacks.success', kind: 'Google'
-        sign_in_and_redirect @user, event: :authentication
-      else
-        session['devise.google_data'] = request.env['omniauth.auth'].except('extra') # Removing extra as it can overflow some session stores
-        redirect_to new_user_registration_url, alert: @user.errors.full_messages.join("\n")
-      end
+    @user = User.from_omniauth(request.env['omniauth.auth'])
+    if @user.persisted?
+      flash[:notice] = I18n.t 'devise.omniauth_callbacks.success', kind: 'Google'
+      sign_in_and_redirect client.authorization_uri.to_s
+    else
+      session['devise.google_data'] = request.env['omniauth.auth'].except('extra') # Removing extra as it can overflow some session stores
+      redirect_to new_user_registration_url, alert: @user.errors.full_messages.join("\n")
+    end
   end
 
   def after_omniauth_failure_path_for(scope)
@@ -29,7 +28,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
       authorization_uri: "https://accounts.google.com/o/oauth2/auth",
       token_credential_uri: "https://oauth2.googleapis.com/token",
       scope: Google::Apis::CalendarV3::AUTH_CALENDAR_READONLY,
-      redirect_uri: callback_url
+      redirect_uri: user_google_oauth2_omniauth_callback_path
     }
   end
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,5 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
+
 end
 # app/controllers/users/omniauth_callbacks_controller.rb:
