826b584058fd2c4a4a5ab5a9bf8cec1ba8bb3fa0
Yain Porter <144638203+yainporter@users.noreply.github.com>
2024-05-10 22:41:00 -0700

    refactor: Make sure that the GoogleCalendarFacade creates all calendars at once

--- app/facades/google_calendar_facade.rb
+++ app/facades/google_calendar_facade.rb
@@ -1,43 +1,39 @@
 class GoogleCalendarFacade
-  attr_reader :mod
+  attr_reader :all_calendars
 
-  def initialize(mod, days_in_advance = nil)
+  def initialize(days_in_advance = nil)
     @calendar_service = days_in_advance ? GoogleCalendarService.new(days_in_advance) : GoogleCalendarService.new
-    @mod = mod
   end
 
-  def filtered_calendar_events
-    create_calendar_events.compact
+  def create_all_calendar_events
+    calendar_events = Hash.new
+
+    calendar_ids.map do |mod, calendar_id|
+      calendar_events[mod] = create_calendar_events(calendar_id).compact
+      # Compact is needed to get rid of nil events in the array because of the if statement
+    end
+
+    calendar_events
   end
 
   private
 
-  def create_calendar_events
-    service_data[:items].map do | event_data |
+  def create_calendar_events(calendar_id)
+    @calendar_service.turing_calendar(calendar_id)[:items].map do |event_data|
       if event_data[:start][:dateTime]
-          CalendarEvent.new(event_data)
+        # if we don't check for the presence of [:dateTime], then the calendar event is not a daily event, but a weekly one
+        CalendarEvent.new(event_data)
       end
     end
   end
 
-  def service_data
-    @calendar_service.turing_calendar(calendar_id)
-  end
-
-  def calendar_id
-    case @mod
-    when "Mod 0"
-      "casimircreative.com_ronr9dk92ndvlhsk03kf8jd2ro@group.calendar.google.com"
-    when "Mod 1"
-      "casimircreative.com_59k8msrrc2ddhcv787vubvp0s4@group.calendar.google.com"
-    when "Mod 2"
-      "casimircreative.com_rps2hg1nfqjih4rcl3gl6s4lpk@group.calendar.google.com"
-    when "Mod 3"
-      "casimircreative.com_e9k9b6n7bok174ilmqbfdr0sc4@group.calendar.google.com"
-    when "Mod 4"
-      "casimircreative.com_c1s3vspg5v09vh5cnnh88dn2nc@group.calendar.google.com"
-    else
-      "casimircreative.com_ronr9dk92ndvlhsk03kf8jd2ro@group.calendar.google.com"
-    end
+  def calendar_ids
+    {
+      "Community Calendar": "casimircreative.com_ronr9dk92ndvlhsk03kf8jd2ro@group.calendar.google.com",
+      "Mod 1": "casimircreative.com_59k8msrrc2ddhcv787vubvp0s4@group.calendar.google.com",
+      "Mod 2": "casimircreative.com_rps2hg1nfqjih4rcl3gl6s4lpk@group.calendar.google.com",
+      "Mod 3": "casimircreative.com_e9k9b6n7bok174ilmqbfdr0sc4@group.calendar.google.com",
+      "Mod 4": "casimircreative.com_c1s3vspg5v09vh5cnnh88dn2nc@group.calendar.google.com"
+    }
   end
 end
--- spec/facades/google_calendar_facade_spec.rb
+++ spec/facades/google_calendar_facade_spec.rb
@@ -1,38 +1,34 @@
 require "rails_helper"
 
 RSpec.describe GoogleCalendarFacade do
-  let(:student_mod){ "Mod 1" }
-  let(:google_calendar_facade){ GoogleCalendarFacade.new("Mod 1") }
+  let(:google_calendar_facade){ GoogleCalendarFacade.new }
 
   describe "initialize" do
     it "holds a User's mod", :vcr do
       expect(google_calendar_facade).to be_a(GoogleCalendarFacade)
-      expect(google_calendar_facade.mod).to eq(student_mod)
     end
   end
 
-  describe "fltered_calendar_events" do
-    it "removes calendar events without a description array", :vcr do
-      events = google_calendar_facade.filtered_calendar_events
-      events.each do | event |
-        expect(event).to be_a CalendarEvent
-        expect(event.event_description).to_not eq(nil)
+  describe "create_all_calendar_events" do
+    it "gets the Calendar data for all Turing Calendars", :vcr do
+      calendar_events = google_calendar_facade.create_all_calendar_events
+
+      expect(calendar_events).to be_a(Hash)
+      
+      calendar_event_keys = [:"Community Calendar", :"Mod 1", :"Mod 2", :"Mod 3", :"Mod 4"]
+
+      expect(calendar_events.keys).to eq(calendar_event_keys)
+      expect(calendar_events[:"Community Calendar"]).to be_an(Array)
+      expect(calendar_events[:"Mod 1"]).to be_an(Array)
+      expect(calendar_events[:"Mod 2"]).to be_an(Array)
+      expect(calendar_events[:"Mod 3"]).to be_an(Array)
+      expect(calendar_events[:"Mod 4"]).to be_an(Array)
+
+      calendar_events.each do |student_mod, calendar_events|
+        calendar_events.each do |event|
+          expect(event).to be_a(CalendarEvent)
+        end
       end
     end
   end
-
-  # describe "#find_thumbnail_url" do
-  #   let(:slides_facade){GoogleCalendarFacade.new({access_token: "ya29.a0Ad52N3_XTssX0q0K53xI8JgVEtndBsHn9i-a5l5VW1T-UBOiMd8FNG1oIfl-5XqBor1Ad-3DD250QfRpjthJWzWUW1x6TQeAs5Ah29VVi98aP3gwHRahwRPkvYkfFOLN6qwqN5lk7zwtSN-5oUdFjMT6n45swGRQDfNqaCgYKAScSARISFQHGX2MiNEtjUg72mTwW7TLlzFczlQ0171"})}
-
-  #   it "finds all the thumbnail ids for a presentation", :vcr do
-  #     thumbnail_url = slides_facade.find_thumbnail_url("1EAYk18WDjIG-zp_0vLm3CsfQh_i8eXc67Jo2O9C6Vuc")
-  #     thumbnail_url = slides_facade.find_thumbnail_url("1EAYk18WDjIG-zp_0vLm3CsfQh_i8eXc67Jo2O9C6Vuc")
-
-  #     expect(thumbnail_url).to be_an(Array)
-  #     expect(thumbnail_url.count).to eq(5)
-
-  #     thumbnail_url2 = slides_facade.find_thumbnail_url("1m6y3LsBI_i42mOQ5p1fpFxwBPV1C7bbSEEeHvmT9iSM")
-  #     expect(thumbnail_url2.count).to eq(32)
-  #   end
-  # end
 end
