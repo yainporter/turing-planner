99490e870d3ea899ed41a15e63c1cd86d212192c
Yain Porter <144638203+yainporter@users.noreply.github.com>
2024-09-29 01:10:33 -0700

    fix: Remove unused RefreshToken model and migration

--- app/models/refresh_token.rb
+++
@@ -1,3 +0,0 @@
-class RefreshToken < ApplicationRecord
-
-end
--- db/migrate/20240927203118_create_refresh_token.rb
+++
@@ -1,9 +0,0 @@
-class CreateRefreshToken < ActiveRecord::Migration[7.1]
-  def change
-    create_table :refresh_tokens do |t|
-      t.string :refresh_token
-
-      t.timestamps
-    end
-  end
-end
